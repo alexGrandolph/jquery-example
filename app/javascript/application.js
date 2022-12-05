8a962d36e36b4f8a02eeba645c3fb6f9753d5fca
Alex George Randolph <alexgeorgerandolph@gmail.com>
2022-12-05 11:05:08 -0700

    import jquery, attatch jquery to window

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import jquery from 'jquery';
+window.jQuery = jquery;
+window.$ = jquery;
+
+$(function() {
+  console.log('cheese')
+})
